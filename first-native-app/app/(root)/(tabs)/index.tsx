b93d82b8f470ca9ec89a0d91f9ac4d4c37f8c99b
mohithchintu <konireddynagamohithreddy@gmail.com>
2024-12-28 07:24:43 +0530

    Main screen and database integration done

--- first-native-app/app/(root)/(tabs)/index.tsx
+++ first-native-app/app/(root)/(tabs)/index.tsx
@@ -1,20 +1,153 @@
-import { Link } from "expo-router";
-import { Text, View } from "react-native";
+import {
+  ActivityIndicator,
+  Button,
+  FlatList,
+  Image,
+  Text,
+  TouchableOpacity,
+  View,
+} from "react-native";
+import { useEffect } from "react";
+import { router, useLocalSearchParams } from "expo-router";
+import { SafeAreaView } from "react-native-safe-area-context";
 
-export default function Index() {
+import icons from "@/constants/icons";
+
+import Search from "@/components/search";
+import Filters from "@/components/filters";
+// import NoResults from "@/components/NoResults";
+import { Card, FeaturedCard } from "@/components/cards";
+
+import { useAppwrite } from "@/lib/useAppwrite";
+import { useGlobalContext } from "@/lib/global-provider";
+// import seed from "@/lib/seed";
+import { getLatestProperties, getProperties } from "@/lib/appwrite";
+
+const Home = () => {
+  const { user } = useGlobalContext();
+
+  const params = useLocalSearchParams<{ query?: string; filter?: string }>();
+
+  const { data: latestProperties, loading: latestPropertiesLoading } = useAppwrite({
+    fn: getLatestProperties
+  })
+
+  const { data: properties, loading, refetch } = useAppwrite({
+    fn: getProperties,
+    params: {
+      filter: params.filter!,
+      query: params.query!,
+      limit: 6
+    },
+    skip: true
+  })
+
+  useEffect(() => {
+    refetch({
+      filter: params.filter!,
+      query: params.query!,
+      limit: 6
+    });
+  }, [params.query, params.filter]);
+  const handleCardPress = (id: string) => router.push(`/properties/${id}`);
+
   return (
-    <View
-      style={{
-        flex: 1,
-        justifyContent: "center",
-        alignItems: "center",
-      }}
-    >
-      <Text className="font-bold text-lg my-10">Welcome to chintu App</Text>
-      <Link href="/sign-in">Sign In</Link>
-      <Link href="/explore">Explore</Link>
-      <Link href="/profile">Profile</Link>
-      <Link href="/properties/1">Property</Link>
-    </View>
+    <SafeAreaView className="h-full bg-white">
+      {/* <Button title="seed" onPress={seed} /> */}
+      <FlatList
+        data={properties}
+        numColumns={2}
+        renderItem={({ item }) => (
+          <Card item={item} onPress={() => handleCardPress(item.$id)} />
+        )}
+        keyExtractor={(item) => item.$id}
+        contentContainerClassName="pb-32"
+        columnWrapperClassName="flex gap-5 px-5"
+        showsVerticalScrollIndicator={false}
+        ListEmptyComponent={
+          loading ? (
+            <ActivityIndicator size="large" className="text-primary-300 mt-5" />
+          ) : (
+            // <NoResults />
+            <View></View>
+          )
+        }
+        ListHeaderComponent={() => (
+          <View className="px-5">
+            <View className="flex flex-row items-center justify-between mt-5">
+              <View className="flex flex-row">
+                <Image
+                  source={{ uri: user?.avatar }}
+                  className="size-12 rounded-full"
+                />
+
+                <View className="flex flex-col items-start ml-2 justify-center">
+                  <Text className="text-xs font-rubik text-black-100">
+                    Good Morning
+                  </Text>
+                  <Text className="text-base font-rubik-medium text-black-300">
+                    {user?.name}
+                  </Text>
+                </View>
+              </View>
+              <Image source={icons.bell} className="size-6" />
+            </View>
+
+            <Search />
+
+            <View className="my-5">
+              <View className="flex flex-row items-center justify-between">
+                <Text className="text-xl font-rubik-bold text-black-300">
+                  Featured
+                </Text>
+                <TouchableOpacity>
+                  <Text className="text-base font-rubik-bold text-primary-300">
+                    See all
+                  </Text>
+                </TouchableOpacity>
+              </View>
+
+              {latestPropertiesLoading ? (
+                <ActivityIndicator size="large" className="text-primary-300" />
+              ) : !latestProperties || latestProperties.length === 0 ? (
+                // <NoResults />
+                <View></View>
+              ) : (
+                <FlatList
+                  data={latestProperties}
+                  renderItem={({ item }) => (
+                    <FeaturedCard
+                      item={item}
+                      onPress={() => handleCardPress(item.$id)}
+                    />
+                  )}
+                  keyExtractor={(item) => item.$id}
+                  horizontal
+                  showsHorizontalScrollIndicator={false}
+                  contentContainerClassName="flex gap-5 mt-5"
+                />
+              )}
+            </View>
+
+            <View className="mt-5">
+              <View className="flex flex-row items-center justify-between">
+                <Text className="text-xl font-rubik-bold text-black-300">
+                  Our Recommendation
+                </Text>
+                <TouchableOpacity>
+                  <Text className="text-base font-rubik-bold text-primary-300">
+                    See all
+                  </Text>
+                </TouchableOpacity>
+              </View>
+
+              <Filters />
+            </View>
+          </View>
+        )}
+      />
+    </SafeAreaView>
   );
-}
+};
+
+export default Home;
--- first-native-app/app/sign-in.tsx
+++ first-native-app/app/sign-in.tsx
@@ -1,11 +1,42 @@
-import { View, Text } from 'react-native'
+import { ScrollView, Image, View, Text, TouchableOpacity, Alert } from 'react-native'
 import React from 'react'
+import { SafeAreaView } from 'react-native-safe-area-context'
+import images from '@/constants/images'
+import icons from '@/constants/icons'
+import { login } from '@/lib/appwrite'
+import { useGlobalContext } from '@/lib/global-provider'
+import { Redirect } from 'expo-router'
 
 const SignIn = () => {
+    const { refetch, loading, isLoggedIn } = useGlobalContext();
+    if (!loading && isLoggedIn) return <Redirect href='/' />
+    const handlelogin = async () => {
+        const result = await login();
+        if (result) {
+            refetch();
+        } else {
+            Alert.alert('Error', "Falied to login")
+        }
+    }
     return (
-        <View>
-            <Text>SignIn</Text>
-        </View>
+        <SafeAreaView className='bg-white h-screen'>
+            <ScrollView contentContainerClassName='h-full'>
+                <Image source={images.onboarding} className='w-full h-4/6' resizeMode='contain' />
+                <View className='px-10'>
+                    <Text className="text-base text-center uppercase font-rubik text-black-200">Welcome to Chintu App</Text>
+                    <Text className="text-3xl text-center font-rubik-bold text-black-300 mt-2">Let's Play!!! This is {'\n'}
+                        <Text className='text-primary-300'>My ERA</Text>
+                    </Text>
+                    <Text className='text-lg font-rubik text-black-200 text-center mt-12'>Login to Chintu with GOOGLE</Text>
+                    <TouchableOpacity onPress={handlelogin} className='bg-white border border-zinc-100  shadow-md shadow-zinc-300 rounded-full w-full py-4 mt-5'>
+                        <View className='flex flex-row items-center justify-center'>
+                            <Image source={icons.google} className='w-5 h-5' resizeMode='contain' />
+                            <Text className='text-lg font-rubik-medium text-black-300 ml-2'>Continue with GOOGLE</Text>
+                        </View>
+                    </TouchableOpacity>
+                </View>
+            </ScrollView>
+        </SafeAreaView>
     )
 }
 
